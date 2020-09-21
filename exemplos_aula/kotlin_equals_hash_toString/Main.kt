bc267eaf137df1eb160dfa9e49c12197977ac50a
Felipe Medeiros <felipe.miranda.mdrs@gmail.com>
2020-09-21 20:25:32 -0300

    Adiciona exemplos da aula equals hash tostring

---
+++ exemplos_aula/kotlin_equals_hash_toString/Main.kt
@@ -0,0 +1,7 @@
+fun main() {
+    val time1 = Time("Fidelização ao cliente", 6)
+    val time2 = Time("Renda fixa", 6)
+
+    println(time1)
+    println(time1 == time2)
+}
---
+++ exemplos_aula/kotlin_equals_hash_toString/Time.kt
@@ -0,0 +1,2 @@
+data class Time(val nome: String,
+           val quantidade: Int)
