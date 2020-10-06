1fc64c01e3179cdb4478d58b2ae3d6c473391862
Felipe Medeiros <felipe.miranda.mdrs@gmail.com>
2020-10-05 22:31:54 -0300

    Update novo_arquivo.kt

--- novo_arquivo.kt
+++ novo_arquivo.kt
@@ -1,6 +1,6 @@
 fun main() {
-    val time1 = Time("Fidelização ao cliente", 6)
-    val time2 = Time("Renda fixa", 6)
+    val time1 = Time("Fidelização ao cliente", 8)
+    val time2 = Time("Renda fixa", 8)
 
     println(time1 - 1)
     println(time1 == time2)
