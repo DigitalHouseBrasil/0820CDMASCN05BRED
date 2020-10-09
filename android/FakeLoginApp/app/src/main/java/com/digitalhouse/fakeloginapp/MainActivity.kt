b196c36f03b9f43e4b15732a8a7470068102c2cb
Felipe Medeiros <felipe.miranda.mdrs@gmail.com>
2020-10-09 15:34:30 -0300

    Adiciona o FakeLoginApp

---
+++ android/FakeLoginApp/app/src/main/java/com/digitalhouse/fakeloginapp/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.digitalhouse.fakeloginapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ android/FakeLoginApp/app/src/main/java/com/digitalhouse/fakeloginapp/users/UserModel.kt
@@ -0,0 +1,3 @@
+package com.digitalhouse.fakeloginapp.users
+
+data class UserModel(val name: String, val email: String, val password: String)
