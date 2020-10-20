4703f850841a2a11706f9690ea96bf6b802efcd3
lucas <lucas.gsantos@live.com>
2020-10-20 12:40:28 -0300

    uma versão do lucas da resolução do Exercicio do projeto RickApp

--- android/RickApp/app/src/main/java/com/digitalhouse/rickapp/MainActivity.kt
+++ android/RickApp/app/src/main/java/com/digitalhouse/rickapp/MainActivity.kt
@@ -1,24 +1,32 @@
 package com.digitalhouse.rickapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.digitalhouse.rickapp.api.IRespostaDaApi
 import com.digitalhouse.rickapp.api.Personagem
 import com.digitalhouse.rickapp.api.RickApi
-import com.squareup.picasso.Picasso
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val viewManager = LinearLayoutManager(this)
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
 
-        RickApi.getData(this, object: IRespostaDaApi {
+        RickApi.getData(this, object : IRespostaDaApi {
             override fun obtevePersonagens(personagens: List<Personagem>) {
-                Picasso.get()
-                        .load(personagens[0].imagemUrl)
-                        .into(minhaImagem)
+                val viewAdapter = RickAdapter(personagens)
+                with(recyclerView) {
+                    setHasFixedSize(true)
+                    layoutManager = viewManager
+                    adapter = viewAdapter
+                    addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
+                }
             }
         })
     }
