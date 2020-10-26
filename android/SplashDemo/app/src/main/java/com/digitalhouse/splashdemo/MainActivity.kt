0bd3e141fc9ecc68b4b414e264aa18d2dbce29fc
Felipe Medeiros <felipe.miranda.mdrs@gmail.com>
2020-10-26 20:52:43 -0300

    Adiciona aula splash scrscreen

---
+++ android/SplashDemo/app/src/main/java/com/digitalhouse/splashdemo/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.digitalhouse.splashdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
